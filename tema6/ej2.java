acb79f725fea64c3395e95444193ac46dbb6399c
carlos-alvarez-moreno <72309389+carlos-alvarez-moreno@users.noreply.github.com>
2020-11-09 10:30:19 +0100

    Add files via upload

---
+++ tema6/ej2.java
@@ -0,0 +1,56 @@
+/*
+ * ej2.java
+ * 
+ * Copyright 2020 Linux User <tux@slitaz>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * @author carlos 
+ */
+
+
+public class ej2 {
+	
+	public static void main (String args[]) {
+    
+    int numcarta= (int)(Math.random()*13)+1;
+    switch(numcarta){
+      default:System.out.println(numcarta);
+      break;
+      case 1 : System.out.println("A");
+      break;
+      case 11: System.out.println("J");
+      break;
+      case 12: System.out.println("Q");
+      break;
+      case 13: System.out.println("K");
+      break;
+
+    }
+    
+    int carta= (int)(Math.random()*5);
+    switch(carta){
+      case 1: System.out.print("de picas");
+      break;
+      case 2: System.out.print("de treboles");
+      break;
+      case 3: System.out.print("de corazones");
+      break;
+      case 4: System.out.print("de diamantes");
+      break;
+    }
+	}
+}
