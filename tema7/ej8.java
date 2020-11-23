73aaad40c47604bf1476e7f863b36f7b0c28e87a
carlos-alvarez-moreno <72309389+carlos-alvarez-moreno@users.noreply.github.com>
2020-11-23 09:41:39 +0100

    Add files via upload

---
+++ tema7/ej8.java
@@ -0,0 +1,42 @@
+/*
+ * ej8.java
+ * 
+ * Copyright 2020 Linux User <tux@slitaz>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * @author carlos
+ */
+import java.util.Scanner;
+
+public class ej8 {
+	
+	public static void main (String args[]) {
+    
+    Scanner s = new Scanner(System.in);
+		int num[] = new int[12];
+    String[] mes = {"enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"};
+    
+    for(int i=0; i<12; i++){
+      System.out.print("\ntemperatura mes "+mes[i]+":");
+      num[i]= s.nextInt();
+      for(int j=0; j<num[i]; j++){
+        System.out.print("*");
+      }
+    }
+	}
+}
+
